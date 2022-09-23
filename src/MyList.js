fc4e4bc6e14e2dc55f997fbdcbb7f35360476fb5
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-22 17:29:31 -0700

    finish deliverable

---
+++ src/MyList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render(){
+    let toDoItems = this.props.theList.map((item, i) =>{
+      return <ListItem
+        task={item}
+        key={`item-${i}`}
+      />
+    })
+    return(
+      <div>
+        <h1>
+          Things I should stop procrastinating
+        </h1>
+
+        <ul>
+            {toDoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
